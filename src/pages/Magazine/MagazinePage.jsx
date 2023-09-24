467c3a2182753e6cf96dae8a9eaedb99027ceeb7
Macbook pro <macbookpro@Macbooks-MacBook-Pro.local>
2023-09-24 14:40:17 +0600

    res magazine

--- src/pages/Magazine/MagazinePage.jsx
+++ src/pages/Magazine/MagazinePage.jsx
@@ -1,29 +1,121 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Box, Button, Grid, Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ban1 from "../../asset/image/test4.jpg";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
 
 const MagazinePage = () => {
   const navigate = useNavigate();
 
   return (
     <Fragment>
-       <Grid container spacing={2}>
-           <Grid item lg={3} md={4} sm={12} xs={12}></Grid>
-           <Grid item lg={9} md={8} sm={12} xs={12}>
-              <div className="content_body">
-                    <Tooltip title="Back">
-                       <div className="backArrow" onClick={(e)=>{navigate(-1)}}>
-                          <ArrowBackIcon/>
-                       </div> 
-                   </Tooltip>
-                  <Box display='flex' justifyContent='center' justifyItems='center'>
-                    <Button disabled><h1>Upcoming...</h1></Button>
-                  </Box>
-               </div>
-           </Grid>
+      <Grid container spacing={2}>
+        <Grid item lg={3} md={4} sm={12} xs={12}></Grid>
+        <Grid item lg={9} md={8} sm={12} xs={12}>
+          <div className="content_body">
+            <Tooltip title="Back">
+              <div
+                className="backArrow"
+                onClick={(e) => {
+                  navigate(-1);
+                }}
+              >
+                <ArrowBackIcon />
+              </div>
+            </Tooltip>
+            <div className="magazine">
+              <div className="magazine_top">
+                <div className="magazine_t">Commuinity Magazine</div>
+                <div className="top_left">
+                  <Button variant="outlined">Category</Button>
+                  <Button variant="contained">Contant</Button>
+                </div>
+              </div>
+              <div className="mag_category_list">
+                <h4>Category List</h4>
+                <ul>
+                  <li> Cat-One</li>
+                  <li> Cat-Two</li>
+                  <li> Cat-Three</li>
+                  <li> Cat-Four</li>
+                  <li> Cat-Five</li>
+                  <li> Cat-Six</li>
+                </ul>
+              </div>
+              <div className="magzine_slider">
+                <Swiper
+                  slidesPerView={1}
+                  spaceBetween={30}
+                  loop={true}
+                  navigation={true}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <div className="magzine_slider_content">
+                      <img src={ban1} alt="" />
+                      <div className="mag_content">
+                        <div className="content_info">
+                          <h2>Slider Heading Is Here</h2>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+              <div className="main_contant">
+                <div className="sec_head">Mian Content</div>
+
+                <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <div className="main_content_item">
+                      <div className="item_img">
+                        <img src={ban1} alt="" />
+                      </div>
+                      <div className="content_title">This Is Content Title</div>
+                      <p>
+                        Lorem ipsum dolor sit amet consec dd tetur, adipisicing
+                        elit.
+                      </p>
+                    </div>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <div className="main_content_item">
+                      <div className="item_img">
+                        <img src={ban1} alt="" />
+                      </div>
+                      <div className="content_title">This Is Content Title</div>
+                      <p>
+                        Lorem ipsum dolor sit amet consec dd tetur, adipisicing
+                        elit.
+                      </p>
+                    </div>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <div className="main_content_item">
+                      <div className="item_img">
+                        <img src={ban1} alt="" />
+                      </div>
+                      <div className="content_title">This Is Content Title</div>
+                      <p>
+                        Lorem ipsum dolor sit amet consec dd tetur, adipisicing
+                        elit.
+                      </p>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </div>
         </Grid>
+      </Grid>
     </Fragment>
   );
 };
